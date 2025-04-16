48beb8038be35fab976e5ac0da628c1368e786c3
chuongminh32 <23110187@student.hcmute.edu.vn>
2025-04-16 15:56:18 +0700

    basic code game flappy bird - minhchuong/16-04-2025

---
+++ MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿//namespace FlappyBirdWinForms
+namespace Flappybird
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label scoreText;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            scoreText = new Label();
+            btnRestart = new Button();
+            bird = new PictureBox();
+            pipeBottom = new PictureBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)bird).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pipeBottom).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 20;
+            gameTimer.Tick += gameTimer_Tick;
+            // 
+            // scoreText
+            // 
+            scoreText.AutoSize = true;
+            scoreText.Font = new Font("Arial", 18F, FontStyle.Bold);
+            scoreText.ForeColor = Color.White;
+            scoreText.Location = new Point(6, 9);
+            scoreText.Name = "scoreText";
+            scoreText.Size = new Size(134, 35);
+            scoreText.TabIndex = 0;
+            scoreText.Text = "Score: 0";
+            // 
+            // btnRestart
+            // 
+            btnRestart.BackColor = Color.Transparent;
+            btnRestart.FlatStyle = FlatStyle.Flat;
+            btnRestart.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRestart.ForeColor = Color.OrangeRed;
+            btnRestart.Location = new Point(338, 218);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(112, 40);
+            btnRestart.TabIndex = 2;
+            btnRestart.Text = "RESTART";
+            btnRestart.UseVisualStyleBackColor = false;
+            btnRestart.Visible = false;
+            btnRestart.Click += button1_Click;
+            // 
+            // bird
+            // 
+            bird.BackColor = Color.Transparent;
+            bird.Image = Properties.Resources.bird1;
+            bird.Location = new Point(100, 100);
+            bird.Name = "bird";
+            bird.Size = new Size(40, 40);
+            bird.TabIndex = 3;
+            bird.TabStop = false;
+            bird.Visible = false;
+            // 
+            // pipeBottom
+            // 
+            pipeBottom.BackColor = Color.Transparent;
+            pipeBottom.Image = Properties.Resources.pipeBottom;
+            pipeBottom.Location = new Point(539, 262);
+            pipeBottom.Name = "pipeBottom";
+            pipeBottom.Size = new Size(60, 200);
+            pipeBottom.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeBottom.TabIndex = 4;
+            pipeBottom.TabStop = false;
+            pipeBottom.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.pipeTop;
+            pictureBox1.Location = new Point(539, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(60, 120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            BackColor = Color.SkyBlue;
+            BackgroundImage = Properties.Resources.background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 500);
+            Controls.Add(pictureBox1);
+            Controls.Add(pipeBottom);
+            Controls.Add(bird);
+            Controls.Add(btnRestart);
+            Controls.Add(scoreText);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Flappy Bird";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)bird).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pipeBottom).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+        private Button btnRestart;
+        private PictureBox bird;
+        private PictureBox pipeBottom;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Flappybird
+{
+    public partial class Form1 : Form
+    {
+        // xoay đâù chim theo hướng bay 
+        float birdAngle = 0f;           // góc quay của chim
+        float maxAngle = 90f;           // góc xoay tối đa khi rơi
+        float minAngle = -25f;          // góc xoay khi bay lên
+
+
+        int gravity = 1;
+        int jump = -12;
+        int velocityY = 0;
+        int birdY;
+
+        int pipeX;
+        int pipeWidth = 60;
+        int pipeHeight;
+        int gap = 200;
+        int pipeSpeed = 6;
+        int score = 0;
+
+
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Giảm flicker khi vẽ pipe
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.background;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            birdY = bird.Top;
+            pipeX = this.Width;
+            pipeHeight = rand.Next(100, this.Height - gap - 100);
+            velocityY = 0;
+
+            gameTimer.Start();
+            this.MouseClick += Form1_MouseClick;
+        }
+
+        private void Form1_MouseClick(object? sender, MouseEventArgs e)
+        {
+            birdAngle = minAngle;
+            velocityY = jump; // reset vận tốc Y để chim bay lên
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // chim rơi
+            velocityY += gravity;
+            birdY += velocityY;
+            bird.Top = birdY;
+
+            // cột di chuyển
+            pipeX -= pipeSpeed;
+
+
+            // Góc xoay dần dần tăng về maxAngle
+            if (birdAngle < maxAngle)
+                birdAngle += 2f;
+
+            // cột đi qua trái thì reset lại
+            if (pipeX < -pipeWidth)
+            {
+                pipeX = this.Width;
+                pipeHeight = rand.Next(100, this.Height - gap - 100);
+                score++;
+            }
+
+            // kiểm tra va chạm
+            Rectangle birdRect = bird.Bounds;
+            Rectangle pipeTop = new Rectangle(pipeX, 0, pipeWidth, pipeHeight);
+            Rectangle pipeBottom = new Rectangle(pipeX, pipeHeight + gap - 40 , pipeWidth, this.Height - pipeHeight - gap);
+
+            if (birdRect.IntersectsWith(pipeTop) || birdRect.IntersectsWith(pipeBottom) ||
+                bird.Bottom >= this.ClientSize.Height - 40)
+            {
+                gameTimer.Stop();
+                btnRestart.Visible = true;
+            }
+
+            // cập nhật điểm và vẽ lại pipe
+            scoreText.Text = "Score: " + score;
+            this.Invalidate();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+
+            // Ống trên (hướng xuống)
+            Image pipeTop = Properties.Resources.pipeUp;
+            g.DrawImage(pipeTop, new Rectangle(pipeX, 0, pipeWidth, pipeHeight));
+
+            // Ống dưới (hướng lên)
+            Image pipeBottom = Properties.Resources.pipeDown;
+            int bottomY = pipeHeight + gap;
+            int bottomHeight = this.Height - bottomY;
+            g.DrawImage(pipeBottom, new Rectangle(pipeX, bottomY - 80 , pipeWidth, bottomHeight));
+
+            // Vẽ chim xoay
+            Bitmap birdImg = Properties.Resources.bird;
+            Bitmap rotatedBird = RotateImage(birdImg, birdAngle);
+            g.DrawImage(rotatedBird, bird.Left, bird.Top, bird.Width, bird.Height);
+
+            // Giải phóng tài nguyên
+            rotatedBird.Dispose();
+        }
+        
+        private Bitmap RotateImage(Bitmap source, float angle)
+        {
+            // Tạo ảnh mới với kích thước đủ lớn để chứa ảnh xoay
+            Bitmap rotated = new Bitmap(source.Width + 30, source.Height + 30);
+            rotated.MakeTransparent(); // đảm bảo nền trong suốt
+
+            using (Graphics g = Graphics.FromImage(rotated))
+            {
+                // Di chuyển gốc tọa độ đến tâm ảnh
+                g.TranslateTransform(source.Width / 2f, source.Height / 2f);
+                // Xoay
+                g.RotateTransform(angle);
+                // Vẽ ảnh gốc, căn giữa
+                g.TranslateTransform(-source.Width / 2f, -source.Height / 2f);
+                g.DrawImage(source, 0, 0);
+            }
+            return rotated;
+        }
+
+
+        private void RestartGame()
+        {
+            bird.Top = this.Height / 2;
+            birdY = bird.Top;
+            velocityY = 0;
+
+            pipeX = this.Width;
+            pipeHeight = rand.Next(100, this.Height - gap - 100);
+            score = 0;
+
+            scoreText.Text = "Score: 0";
+
+            birdAngle = 0f;
+            gameTimer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+            btnRestart.Visible = false;
+        }
+    }
+}
